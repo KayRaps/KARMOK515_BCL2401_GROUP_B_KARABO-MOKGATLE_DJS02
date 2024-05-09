b7e6cb1072e5805aaccc737ec57e52fc6402adcc
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-09 16:07:29 +0200

    used a regex function to check if either the dividend or the divider has numeric characters and not any other characters, it matches any string that consists entirely of digits.

--- scripts.js
+++ scripts.js
@@ -12,4 +12,12 @@ form.addEventListener("submit", (event) => {
     result.innerText = "Division not performed. Both values are required in inputs. Try again.";
     return;
   }
+
+  // Error handling: checking if inputs contain invalid characters
+  const validInputRegex = /^[0-9]+$/;
+  if (!validInputRegex.test(dividend) || !validInputRegex.test(divider)) {
+    result.classList.add("critical-error")
+    result.innerText = "Something critical went wrong. Please reload the page.";
+    return;
+  }
 });
