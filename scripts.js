7c3ee36bd6ab0ed5d20d1f0328928169d2927ff7
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-05-09 15:59:56 +0200

    checking if inputs are empty.

--- scripts.js
+++ scripts.js
@@ -6,4 +6,10 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  // Error handling: checking if inputs are empty
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
 });
